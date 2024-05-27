ee229c4d782d2fdc2f7b57e31125b55aeb995da0
tennga <tenngawang6080@gmail.com>
2024-05-27 17:57:27 -0400

    hobby icon added

--- src/containers/main-section/MainHobby.tsx
+++ src/containers/main-section/MainHobby.tsx
@@ -1,11 +1,32 @@
 import Card from "@/components/Card.tsx";
+import CoffeeIcon from "../../public/images/coffee.png"
+import MusicIcon from "../../public/images/headphone.png"
+import DuckIcon from "../../public/images/duck.png"
+import Image from 'next/image'
 
 function MainHobby(){
-    const valueArr = [1,2,3];
-    const renderCards = valueArr.map(hobby => {
+    const hobbies = [
+        {
+            title: 'Coffee',
+            body: 'Keeps Me Awake',
+            icon: CoffeeIcon
+        },
+        {
+            title: 'Music',
+            body: 'Stimulate Creativity',
+            icon: MusicIcon
+        },
+        {
+            title: 'Debugger',
+            body: 'Identify Problems',
+            icon: DuckIcon
+        }
+    ];
+    const renderCards = hobbies.map(hobby => {
         return <Card> 
-                <h1>hobby</h1>
-                <p>Sub heading</p>
+                <Image src={hobby.icon} alt={hobby.title} />
+                <h1>{hobby.title}</h1>
+                <p>{hobby.body}</p>
             </Card>
     })
     return(
