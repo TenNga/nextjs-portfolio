11fab05e0053b67c776cdcf49b5db88aef581359
tennga <tenngawang6080@gmail.com>
2024-05-30 18:31:56 -0400

    added chart

--- src/containers/main-section/HistoryTimeline.tsx
+++ src/containers/main-section/HistoryTimeline.tsx
@@ -1,6 +1,70 @@
+'use client'
+
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 function HistoryTimeline() {
+
+    const data = [
+  {
+    name: 'Page A',
+    uv: 1000,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    amt: 2100,
+  },
+];
     return(
-        <div>History horizontal timeline</div>
+        <div className='h-[450px]'>
+            <h1>History Timeline</h1>
+            <ResponsiveContainer width="100%" height="100%">
+                <LineChart
+                width={500}
+                height={300}
+                data={data}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+                >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" strokeDasharray="3 4 5 2" />
+                </LineChart>
+            </ResponsiveContainer>
+        </div>
     )
 };
 
