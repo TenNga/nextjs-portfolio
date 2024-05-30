89ea839b4650f3f983f78a4f9ed6fb05820b9387
Kevin Karma <tenngawang6080@gmail.com>
2024-05-30 12:20:04 -0400

    added tooltip using nextUI and change next to a tag

--- src/components/SkillHobby/ItemLink.tsx
+++ src/components/SkillHobby/ItemLink.tsx
@@ -1,11 +1,18 @@
 import { SkillHobbyType } from "@/types";
 import Image from "next/image";
-import Link from "next/link";
+import {Tooltip} from "@nextui-org/react";
 
 export const ItemLink = ({item}: {item: SkillHobbyType}) => {
     return(
-        <Link className="grow shrink-0 basis-3/12" key={item.title} href={item.link} rel={`${item.title} links`}>
-            <Image src={item.icon} alt={item.title} width={52} height={52}/>
-        </Link>
+        <Tooltip key={item.title} content={item.title} color="primary" classNames={{
+            content: [
+              "py-1 px-3",
+              "text-black bg-white rounded",
+            ],
+          }}>
+            <a className="grow shrink-0 basis-3/12" href={item.link} rel={`${item.title} links`} target="_blank">
+                <Image src={item.icon} alt={item.title} width={52} height={52}/>
+            </a>
+        </Tooltip>
     )
 }
